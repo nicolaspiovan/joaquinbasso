13058c22936f76f61f45a59b059d58c072927c44
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-02-11 22:42:27 -0300

    Rediseño de index e inicio de carptura de formulario en php

---
+++ envio.php
@@ -0,0 +1,14 @@
+<?php
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    echo $name;
+    echo '<br>';
+    echo $email;
+    echo '<br>';
+    echo $phone;
+    echo '<br>';
+    echo $message;
